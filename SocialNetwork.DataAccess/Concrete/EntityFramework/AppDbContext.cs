43a90795e50730619c5fb3a3340f2e6f57a3c2c3
GumruGurbanali07 <qurbaneliyevaqumru@gmail.com>
2023-09-28 14:23:43 +0400

    commited

---
+++ SocialNetwork.DataAccess/Concrete/EntityFramework/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿
+using SocialNetwork.Core.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Entities.Concrete;
+using SocialNetwork.Core.Entities.Concrete;
+
+namespace SocialNetwork.Core.Configuration
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DatabaseConfiguration.ConnectionString);
+        }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<FriendList> FriendLists { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Reaction> Reactions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<AppUserRole> AppUserRoles { get; set; }
+
+    }
+}
